bac93c8431042b0fba112b093fa70e66c2b22959
MH <Mike@joergensen.eu>
2019-06-18 12:45:07 +0200

    rettelse
    rettelse1

--- testing.cs
+++ testing.cs
@@ -14,7 +14,7 @@ namespace Program
             num = int.Parse(Console.ReadLine());
             while (num != 0)
             {
-                string s = "Hej du dig der sidder og læser dette";
+                string s = "Hej du digsa";
                 r = num % 10;
                 num = num / 10;
                 sum = sum + r;
