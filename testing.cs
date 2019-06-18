0b576d6111e74280a3cdcfe736ca4dae39ab5407
MH <Mike@joergensen.eu>
2019-06-18 12:54:57 +0200

    asd

--- testing.cs
+++ testing.cs
@@ -10,7 +10,7 @@ namespace Program
         static void Main(string[] args)
         {
             int num, sum = 0, r;
-            Console.WriteLine("Enter a Numasdsadsa : ");
+            Console.WriteLine("Enter a Numdsadsadsa : ");
             num = int.Parse(Console.ReadLine());
             while (num != 0)
             {
