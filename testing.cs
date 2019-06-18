bb89e1b2cff7eb3925b614ee86da7e31dc02520f
MH <Mike@joergensen.eu>
2019-06-18 12:40:32 +0200

    D

--- testing.cs
+++ testing.cs
@@ -14,7 +14,7 @@ namespace Program
             num = int.Parse(Console.ReadLine());
             while (num != 0)
             {
-                string s = "Hej du";
+                string s = "Hej du dig der";
                 r = num % 10;
                 num = num / 10;
                 sum = sum + r;
